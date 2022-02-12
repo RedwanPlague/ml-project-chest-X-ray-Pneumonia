958f98840b53f88786332919646dfc901d23ac81
RedwanPlague <mdredwan.rh@gmail.com>
2022-02-12 11:31:01 +0600

    testing code

--- src/test.py
+++ src/test.py
@@ -13,31 +13,27 @@ dirs = [
     'dummy_data/test/PNEUMONIA',
 ]
 
+rgb_cnt, l_cnt = 0, 0
+
 for dir in dirs:
     # hs, ws = [], []
-
     for img_file in os.listdir(dir):
         img = Image.open(os.path.join(dir, img_file))
         if len(img.getbands()) == 3:
-            print('-------------')
+            rgb_cnt += 1
             img_arr = np.array(img).transpose((2, 0, 1))
-            print(img_arr.shape)
-            print(np.isclose(img_arr[0], img_arr[1]).all())
-            print(np.isclose(img_arr[0], img_arr[2]).all())
-            print(np.isclose(img_arr[1], img_arr[2]).all())
-            im2 = img.convert('L')
-            im2_arr = np.array(im2)
-            print(im2_arr.shape)
-            print(np.isclose(im2_arr, img_arr[1]).all())
-            print(np.isclose(im2_arr, img_arr[2]).all())
-            print(np.isclose(im2_arr, img_arr[2]).all())
-            print('-------------')
+            r, g, b = img_arr
+            diff = np.abs(r - g).sum() + np.abs(g - b).sum() + np.abs(b - r).sum()
+            if diff != 0:
+                print(diff)
             # im2 = Image.open('check.jpeg').convert('RGB')
             # if ImageChops.difference(img, im2).getbbox():
             #     print('diff')
             # else:
             #     print('same')
             # assert False
+        else:
+            l_cnt += 1
         # h, w, c = img.size
         # assert c == 3
         # hs.append(h)
@@ -45,3 +41,5 @@ for dir in dirs:
 
     # print(f'{min(hs)} {max(hs)} {sum(hs)/len(hs)}')
     # print(f'{min(ws)} {max(ws)} {sum(ws)/len(ws)}')
+
+print(rgb_cnt, l_cnt)
